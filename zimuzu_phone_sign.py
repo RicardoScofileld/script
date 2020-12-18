29b6eb02852f2402f3a8a07c0cf02166f2443586
ricardoscofileld <drqinghuan@163.com>
2020-12-18 11:17:03 +0800

    token有时限，需定时更新，登录失败发送邮件提醒

--- zimuzu_phone_sign.py
+++ zimuzu_phone_sign.py
@@ -7,6 +7,9 @@ Description: 手机端每日签到, 抓包获取uid和token
 '''
 
 import time
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
 import requests
 
 UID = "终端uid"
@@ -21,9 +24,30 @@ HEADERS = {
 
 session = requests.Session()
 
+
+class Mail():
+
+    def __init__(self, host, user, passwd):
+        self.smtp = smtplib.SMTP_SSL(host, 465)
+        self.user = user
+        self.smtp.login(user, passwd)
+
+    def send_mail(self, receivers, title, content):
+        message = MIMEText(content, "plain", "utf-8")
+        message["From"] = self.user
+        message["To"] = receivers
+        message["Subject"] = title
+        try:
+            self.smtp.sendmail(self.user, receivers, message.as_string())
+        except Exception as e:
+            print(e)
+
+
+
 def login():
     res = session.get(url=LOGIN_URL, headers=HEADERS)
     print(res.json())
+    return res.json()["status"]
 
 def sign():
     res = session.get(url=SIGN_URL, headers=HEADERS)
@@ -33,6 +57,9 @@ def sign():
 
 if __name__ == "__main__":
     # login get session
-    login()
+    status_code = login()
+    if status_code != 1:
+        email = Mail("smtp.163.com", "邮箱账号", "邮箱密码")
+        email.send_mail("接受邮箱账号", "人人字幕签到失败", "请及时更新token值")
     # sign in 
     sign()
